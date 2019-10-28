76d7896a91ce5516cd8f260fdeb37c5b53128752
afiru <n.soga@sunpla.com>
2019-10-28 18:39:01 +0900

    spa関連の書き込み

--- dist/js/main.js
+++ dist/js/main.js
@@ -371,7 +371,7 @@ const modal = function() {
 
 		==================*/
 
-		const openButtons = document.querySelectorAll('[data-target]')
+		const openButtons = document.querySelectorAll('button[data-target]')
 		// const openButtons = document.querySelectorAll('button.js-openModal')
 		// const openButtonTargets = Array.prototype.forEach.call(
 		// 	openButtons,
@@ -788,6 +788,54 @@ const wpWpFormYubin = function() {
 
 wpWpFormYubin()
 
+/*=================================================
+
+SPAのアニメーション用
+
+==================================================*/
+
+const spaAnimation = function() {
+	try {
+		const linkButtons = document.querySelectorAll('button.open')
+		for (const linkButton of linkButtons) {
+			// ボタンをクリックしたら
+			linkButton.addEventListener('click', event => {
+				// クリックされた要素
+				const element = event.currentTarget
+				// 対象のIDをdata-target属性から取得
+				const target = element.dataset.target
+
+				// 遷移リンクのshowクラスを追加
+				const link = document.querySelector(`.link-${target}`)
+				link.classList.add('show')
+
+				// 遷移先画面のshowクラスを追加
+				const detail = document.querySelector(`.detail-${target}`)
+				detail.classList.add('show')
+			})
+		}
+		// 閉じるボタンを取得
+		const closeButtons = document.querySelectorAll('button.close')
+		for (const button of closeButtons) {
+			// ボタンをクリックしたら
+			button.addEventListener('click', () => {
+				// 遷移リンクのshowクラスを除去
+				const links = document.querySelectorAll('.link')
+				for (const link of links) {
+					link.classList.remove('show')
+				}
+
+				// 遷移先画面のshowクラスを除去
+				const details = document.querySelectorAll('.detail')
+				for (const detail of details) {
+					detail.classList.remove('show')
+				}
+			})
+		}
+	} catch (error) {}
+}
+
+spaAnimation()
 
 /*=================================================
 
